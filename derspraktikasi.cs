f8e0ff76572082eb67522efc12c23d729a51df42
RustamCode <100156400+RustamCode@users.noreply.github.com>
2022-03-11 17:37:44 +0400

    10.03 praktika

---
+++ derspraktikasi.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int qiymetler = SumMassiveElements(1, 2, 3, 4);
+            //Console.WriteLine(qiymetler);
+            //Console.WriteLine(Subtract(10));
+            Console.WriteLine(Sum(5,6,3));
+            
+        }
+        static int SumMassiveElements(params int[] arr)
+        {
+            int sum = 0;
+            foreach( var item in arr)
+            {
+                sum += item;
+            }
+            return sum;
+        }
+        static int Subtract( int num1 , int num2=1)
+        {
+            return num1 - num2;
+        }
+        static int Sum( int num1)
+        {
+            return 1;
+        }
+        static int Sum( params int[] arr)
+        {
+            return 0;
+        }
+    }
+}
